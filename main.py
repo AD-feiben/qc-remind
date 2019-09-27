51722ae58f946e6489686e9be6aed05168198416
ShiRunPei <feiben.dev@gmail.com>
2019-09-27 19:38:12 +0800

    添加定时任务
    添加定时任务

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import config
 import requests
 import json
-from utils import mail
 import logging
+from utils import mail
+from datetime import datetime
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+persons = config.persons
 
 
 def get_otc_data():
@@ -21,29 +25,39 @@ def get_otc_data():
     r = requests.get(config.url, params=params, headers=headers)
     try:
         res = json.loads(r.text)
-        return res['datas']
+        return res.get('datas')
     except Exception as e:
         logging.error(e)
 
 
 def task():
     res = get_otc_data()
-    if res is not None:
-        otc_ad = res['list'][0]
-        price = otc_ad['price']
-        min_money = otc_ad['minMoney']
-        max_money = otc_ad['maxMoney']
-        nick_name = otc_ad['nickName']
-        remain_amount = otc_ad['remainAmount']
-        f_price = float(price)
+    if res is None:
+        return
+
+    otc_ad = res.get('list')[0]
+    price = otc_ad.get('price')
+    min_money = otc_ad.get('minMoney')
+    max_money = otc_ad.get('maxMoney')
+    nick_name = otc_ad.get('nickName')
+    remain_amount = otc_ad.get('remainAmount')
+    f_price = float(price)
 
     remind = '【QC-Remind】当前 QC 价格：{}, 商家：{}, 最低成交：{}, 最高成交：{}, 剩余：{}'\
         .format(price, nick_name, min_money, max_money, remain_amount)
 
-    for p in config.persons:
+    for p in persons:
         if f_price >= p['higher'] or f_price <= p['lower']:
-            mail.send_mail(p['email'], 'QC 价格 {}'.format(price), remind)
+            now = datetime.now().timestamp()
+            if (p.get('timestamp') is None) or (now - p.get('timestamp') > p.get('recheck') * 60):
+                p['timestamp'] = now
+                mail.send_mail(p['email'], 'QC 价格 {}'.format(price), remind)
 
 
 if __name__ == '__main__':
-    task()
+    scheduler = BlockingScheduler()
+    scheduler.add_job(task, 'cron', hour='8-23', minute='*/1')
+    try:
+        scheduler.start()
+    except Exception as e:
+        pass
